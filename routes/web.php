8d1fc84003dcec8061e8a243e830cde65ea65e10
Claudio Ferraro <claudio@justbetter.nl>
2024-06-14 11:12:06 +0200

    Add named route for order reminders account index page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Rapidez\OrderReminder\Http\Controllers\OrderReminderController;
 
 Route::middleware('web')->group(function () {
-    Route::view('account/order-reminders', 'rapidez-order-reminder::account.order-reminders');
+    Route::view('account/order-reminders', 'rapidez-order-reminder::account.order-reminders')->name('rapidez-order-reminder.account.index');
     Route::controller(OrderReminderController::class)->group(function () {
         Route::get('order_reminders/confirm/{orderReminder}', 'confirm')->name('rapidez-order-reminder.confirm')->middleware('signed');
         Route::get('order_reminders/unsubscribe/{orderReminder}', 'unsubscribe')->name('rapidez-order-reminder.unsubscribe')->middleware('signed');
